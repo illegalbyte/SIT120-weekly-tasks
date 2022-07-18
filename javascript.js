eca3dd78e191d67e5d14945c2c9feea8175cc0be
Lewis Gentle <lewis.gentle@gmail.com>
2022-07-18 19:02:15 +1000

    install vue

--- javascript.js
+++ javascript.js
@@ -2,3 +2,9 @@ function gettime() {
 	document.getElementById("timebutton").innerHTML = "The time is " + new Date().toLocaleTimeString();
 }
 
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: 'Hello Vue!'
+	}
+})
